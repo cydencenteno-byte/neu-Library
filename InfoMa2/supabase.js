c4a9030f6857a89f212395f68d824e794d8b9746
Cyden Centeno <cydencenteno@gmail.com>
2026-03-16 00:00:22 +0800

    update supabase key

--- InfoMa2/supabase.js
+++ InfoMa2/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
 
 const supabaseUrl = 'https://kyknrovufypueibvmkas.supabase.co'
-const supabaseKey = 'sb_publishable_2Agdz5TqVj_YcZN7TvNEDg_oZL5HGWE'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt5a25yb3Z1ZnlwdWVpYnZta2FzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM1NDE3MzAsImV4cCI6MjA4OTExNzczMH0.fqNMkSi2Ye_In8XIlkzDKqn15GJMhZ-_42yUnwOEj0Y'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
